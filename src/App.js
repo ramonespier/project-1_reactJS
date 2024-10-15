47ddd99f1a3d296b82ace2c1bc7fd28c918fc9ac
ramonespier <ramon.coelhomelo@outlook.com>
2024-10-15 09:05:05 -0300

    reactJS: aprendendo states e diminuindo constructor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // constructor(props) { // nao preciso do constructor, posso eliminar ele tambem
+    // super(props);
+    // this.handlePCLick = this.handlePCLick.bind(this) // desnecessário, ultrapassado // arrow function resolve
+    // this.state = {tksmckmsk}
+      state = {
+      name: 'Ramon Coelho',
+      counter: 0
+    };
+
+  handlePCLick = () => {
+    this.setState({name: "sonatA #chery"});
+  }
+  
+  handleAClick = (event) => {
+    event.preventDefault();
+    const {counter} = this.state;
+    this.setState({counter: counter + 1});
+  }
+
+  render() {
+    const {name, counter} = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePCLick}>
+            {name} {counter}
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Contador
+          </a> 
+        </header>
+      </div>
+    ); // single page application
+  }
 }
+
 
 export default App;
