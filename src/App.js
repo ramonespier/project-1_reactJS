bc179ea45f5ad269efd4b2755859ae050553ed20
ramonespier <ramon.coelhomelo@outlook.com>
2024-10-22 19:50:14 -0300

    feat: lifecycle methods

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 class App extends Component {
   state = {
+    counter: 0,
     posts: [
       {
         id: 1,
@@ -22,15 +23,64 @@ class App extends Component {
     ]
   };
 
+  // lifecycle method
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.setState({
+  //     posts: [
+  //       {
+  //         id: 1,
+  //         title: 'Titulo 1',
+  //         body: 'Corpo 1'
+  //       },
+  //       {
+  //         id: 2,
+  //         title: 'Titulo 2',
+  //         body: 'Corpo 2'
+  //       },
+  //       {
+  //         id: 3,
+  //         title: 'Titulo 3',
+  //         body: 'Corpo 3'
+  //       }
+  //     ]
+  //    });}, 5000);
+
+  // }
+
+  timeOutUpdate = null
+
+  componentDidMount() {
+    this.handleTimeOut();
+  }
+
+  componentDidUpdate() {
+    this.handleTimeOut();
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.timeOutUpdate);
+  }
+
+  handleTimeOut = () => {
+    const { posts, counter } = this.state;
+    posts[0].title = 'Titulo alterado';
+
+
+    this.timeOutUpdate = setTimeout(() => {
+      this.setState({ posts, counter: counter + 1 })
+    }, 500);
+  }
+
   render() { //na render eu so posso retornar 1 elemento root (nesse caso, App); a menos que eu use fragmentos (<> </>)
-    const { posts } = this.state;
+    const { posts, counter } = this.state;
 
     return (
       <div className="App">
+        <h1>{counter}</h1>
         {posts.map(post => (
 
           <div key={post.id}>
-
             <h1 >{post.title}</h1>
             <p>{post.body}</p>
 
