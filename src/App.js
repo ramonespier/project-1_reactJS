7509e2a53633a08249d89fdfbaf50f6234802664
ramonespier <ramon.coelhomelo@outlook.com>
2024-12-16 08:44:10 -0300

    aprendendo data fetching

--- src/App.js
+++ src/App.js
@@ -3,26 +3,24 @@ import './App.css';
 
 class App extends Component {
   state = {
-    counter: 0,
-    posts: [
-      {
-        id: 1,
-        title: 'Titulo 1',
-        body: 'Corpo 1'
-      },
-      {
-        id: 2,
-        title: 'Titulo 2',
-        body: 'Corpo 2'
-      },
-      {
-        id: 3,
-        title: 'Titulo 3',
-        body: 'Corpo 3'
-      }  // hard coded
-    ]
+    posts: []
   };
 
+  // {
+  //   id: 1,
+  //   title: 'Titulo 1',
+  //   body: 'Corpo 1'
+  // },
+  // {
+  //   id: 2,
+  //   title: 'Titulo 2',
+  //   body: 'Corpo 2'
+  // },
+  // {
+  //   id: 3,
+  //   title: 'Titulo 3',
+  //   body: 'Corpo 3'
+  // }  // hard coded
   // lifecycle method
   // componentDidMount() {
   //   setTimeout(() => {
@@ -48,47 +46,60 @@ class App extends Component {
 
   // }
 
-  timeOutUpdate = null
-
   componentDidMount() {
-    this.handleTimeOut();
+    this.loadPosts();
+
+  }
+
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    });
+
+    this.setState({ posts: postsAndPhotos })
+
+
   }
 
   componentDidUpdate() {
-    this.handleTimeOut();
+
   }
 
   componentWillUnmount() {
-    clearTimeout(this.timeOutUpdate);
+
   }
 
-  handleTimeOut = () => {
-    const { posts, counter } = this.state;
-    posts[0].title = 'Titulo alterado';
-
-
-    this.timeOutUpdate = setTimeout(() => {
-      this.setState({ posts, counter: counter + 1})
-    }, 1500);
-  }
 
   render() { //na render eu so posso retornar 1 elemento root (nesse caso, App); a menos que eu use fragmentos (<> </>)
-    const { posts, counter } = this.state;
+    const { posts } = this.state;
 
     return (
-      <div className="App">
-        <h1>{counter}</h1>
-        {posts.map(post => (
+      <section className='container'>
+        <div className="posts">
+          {posts.map(post => (
+            <div className='post'>
 
-          <div key={post.id}>
-            <h1 >{post.title}</h1>
-            <p>{post.body}</p>
+              <img src={post.cover} alt={post.title} />
+              <div key={post.id} className='post-content'>
+                <h1 >{post.title}</h1>
+                <p>{post.body}</p>
+              </div>
 
-          </div>
+            </div>
 
-        ))}
 
-      </div>
+          ))}
+
+        </div>
+      </section>
       // sempre que eu iterar elementos no jsx, eu itero vários elementos. O react precisa saber de forma rapida e ir diretamente no elemento desejado
     );
   }
